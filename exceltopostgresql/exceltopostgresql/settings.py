d624815051b35b772025e346eb578b4723da4b66
Николай С <nicksrnk@gmail.com>
2025-07-15 08:09:03 +0500

    Добавлена собранная клиентская часть

--- exceltopostgresql/exceltopostgresql/settings.py
+++ exceltopostgresql/exceltopostgresql/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "corsheaders",
     'app'
 ]
 
@@ -58,7 +59,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'exceltopostgresql.urls'
 
